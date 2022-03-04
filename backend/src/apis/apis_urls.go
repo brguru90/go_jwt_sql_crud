5d2a7c0e26bb4b53576d5105624a1f0bf710c9cb
Guruprasad BR <brguru90@gmail.com>
2022-03-04 18:11:46 +0530

    DB connection

--- backend/src/apis/apis_urls.go
+++ backend/src/apis/apis_urls.go
@@ -4,10 +4,11 @@ import (
 	"learn_go/src/apis/views"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 // only the functions whose initial letter is upper case only those can be exportable from package
-func InitApiTest(router *gin.RouterGroup) {
+func InitApiTest(router *gin.RouterGroup, db_connection *pgxpool.Pool) {
 	router.Use(ApiSpecificMiddleware())
 	router.GET("test/:id", test_api)
 	router.GET("hello/", views.Hello_api)
--- backend/src/main.go
+++ backend/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,22 +9,31 @@ import (
 
 	"learn_go/src/apis"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 var all_router *gin.Engine
 
 var SERVER_PORT string = "8000"
 
+// https://github.com/gin-gonic/gin
+
+var db_connection *pgxpool.Pool
+
 func main() {
 	// all_router = gin.New()
 	all_router = gin.Default()
 	all_router.Use(static.Serve("/", static.LocalFile("./src/static", true)))
 
+	db_connection = connect_postgres()
+
 	{
 		// just grouping, to make it more readable
 		api_router := all_router.Group("/api")
+		all_router.Use(cors.Default())
 
 		// an example for global middleware
 		api_router.Use(FindUserAgentMiddleware())
@@ -33,7 +43,7 @@ func main() {
 		})
 
 		// more apis imported
-		apis.InitApiTest(api_router)
+		apis.InitApiTest(api_router, db_connection)
 	}
 
 	if os.Getenv("SERVER_PORT") != "" {
@@ -44,6 +54,25 @@ func main() {
 	bind_to_host := fmt.Sprintf(":%s", SERVER_PORT) //formatted host string
 	all_router.Run(bind_to_host)
 
+}
+
+func connect_postgres() *pgxpool.Pool {
+
+	var DB_USER string = os.Getenv("DB_USER")
+	var DB_PASSWORD string = os.Getenv("DB_PASSWORD")
+	var DB_HOST string = os.Getenv("DB_HOST")
+	var DATABASE string = os.Getenv("DATABASE")
+	var DB_PORT string = os.Getenv("DB_PORT")
+
+	var DB_URL string = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DATABASE)
+
+	dbpool, err := pgxpool.Connect(context.Background(), DB_URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		fmt.Println("DB_URL ==> ", DB_URL)
+		os.Exit(1)
+	}
+	return dbpool
 }
 
 func HeaderHandlerFunc(c *gin.Context) {
