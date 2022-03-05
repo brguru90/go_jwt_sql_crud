d787a78cd41028134c6333d4b7324229e7cfdacb
Guruprasad BR <brguru90@gmail.com>
2022-03-05 13:46:05 +0530

    logger integrated

--- backend/src/my_modules/logger.go
+++ backend/src/my_modules/logger.go
@@ -1,16 +1,23 @@
 package my_modules
 
 import (
+	"os"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func InitLogger() {
 	log.SetLevel(log.DebugLevel)
-	// log.SetFormatter(&log.JSONFormatter{})
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp:          true,
-		DisableLevelTruncation: true,
-		PadLevelText:           true,
-	})
+
+	if os.Getenv("APP_ENV") == "production" {
+		log.SetFormatter(&log.JSONFormatter{})
+	} else {
+		log.SetFormatter(&log.TextFormatter{
+			FullTimestamp:          true,
+			DisableLevelTruncation: true,
+			PadLevelText:           true,
+		})
+	}
+
 	log.SetReportCaller(true)
 }
