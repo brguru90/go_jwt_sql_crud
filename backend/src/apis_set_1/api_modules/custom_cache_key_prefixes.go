a2afb5cee559e66fad684ff2d3b89501f1b6b39b
Guruprasad BR <brguru90@gmail.com>
2022-04-16 01:39:19 +0530

    cache tested as different parallel user

--- backend/src/apis_set_1/api_modules/custom_cache_key_prefixes.go
+++ backend/src/apis_set_1/api_modules/custom_cache_key_prefixes.go
@@ -8,11 +8,12 @@ import (
 )
 
 func ForUserPagination(c *gin.Context) string {
+	
+	payload, ok := my_modules.ExtractTokenPayload(c)
 	if c.Query("page") != "" {
 		// admin uuid could have been return,if access level is implemented
-		return "paginated"
+		return "paginated_uuid="+ payload.Data.UUID // for debugging using normal user id
 	}
-	payload, ok := my_modules.ExtractTokenPayload(c)
 	if !ok {
 		// log.Panicln("Error in extracting payload from token")
 		return "uuid=" + time.Now().String()
