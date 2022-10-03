3a0fbfafef5a6c787f71df4af509e43fddc3a45c
Guruprasad BR <brguru90@gmail.com>
2022-10-03 11:52:05 +0530

    encrypted data in jwt

--- backend/src/my_modules/authentication.go
+++ backend/src/my_modules/authentication.go
@@ -12,10 +12,15 @@ import (
 	"strconv"
 	"time"
 
+	"crypto/cipher"
+	"crypto/aes"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
+
+var EncryptionKey = "thisis32bitlongpassphraseimusing"
+var encryption_bytes_padding = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
 
 type TokenPayload struct {
 	Email string `json:"email" binding:"required"`
@@ -23,6 +28,7 @@ type TokenPayload struct {
 }
 type AccessToken struct {
 	Data TokenPayload `json:"data" binding:"required"`
+	EncryptedData string       `json:"encrypted_data" binding:"required"`
 	// Data         interface{}  `json:"data" binding:"required"`
 	Uname        string `json:"uname" binding:"required"`
 	Token_id     string `json:"token_id" binding:"required"`
@@ -42,6 +48,50 @@ func randomBytes(size int) (blk []byte, err error) {
 	return
 }
 
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+func Decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+// Encrypt method is to encrypt or hide any classified text
+func EncryptAES(key string, text string) string {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		if err!=nil{
+			log.WithFields(log.Fields{
+				"Error": err,
+			}).Panic("NewCipher")		
+		}
+	}
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, encryption_bytes_padding)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+	return Encode(cipherText)
+}
+
+func DecryptAES(key string, text string) string {	
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		if err!=nil{
+			log.WithFields(log.Fields{
+				"Error": err,
+			}).Panic("NewCipher")		
+		}
+	}
+	cipherText := Decode(text)
+	cfb := cipher.NewCFBDecrypter(block, encryption_bytes_padding)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+	return string(plainText)
+}
+
 func GenerateAccessToken(uname string, csrf_token string, data TokenPayload) (string, AccessTokenClaims) {
 
 	time_now := time.Now().UnixMilli()
@@ -51,14 +101,17 @@ func GenerateAccessToken(uname string, csrf_token string, data TokenPayload) (st
 		token_id = data.UUID + "_" + base64.StdEncoding.EncodeToString(_rand) + "_" + strconv.Itoa(int(time_now))
 	}
 
+	data_to_encrypt:="some data"
+
 	var accessTokenPayload AccessTokenClaims = AccessTokenClaims{
 		AccessToken: AccessToken{
 			Uname:        uname,
 			Token_id:     token_id,
 			Data:         data,
+			EncryptedData: EncryptAES(EncryptionKey, data_to_encrypt),	
 			IssuedAtTime: time_now,
 			Exp:          time_now + configs.EnvConfigs.JWT_TOKEN_EXPIRE_IN_MINS*60*1000,
-			Csrf_token:   csrf_token,
+			Csrf_token:    EncryptAES(EncryptionKey, csrf_token),
 		},
 	}
 
@@ -137,7 +190,7 @@ func Authenticate(c *gin.Context, newUserRow NewUserRow) AccessToken {
 	return access_token_payload.AccessToken
 }
 
-func LoginStatus(c *gin.Context, enforce_csrf_check bool) (AccessToken, string, int, bool) {
+func LoginStatus(c *gin.Context,enforce_csrf_check bool) (AccessToken, string, int, bool) {
 	var token_claims AccessTokenClaims
 	access_token, err := c.Cookie("access_token")
 
@@ -189,20 +242,22 @@ func LoginStatus(c *gin.Context, enforce_csrf_check bool) (AccessToken, string, 
 			Column_email: token_claims.AccessToken.Data.Email,
 			Column_uuid:  token_claims.AccessToken.Data.UUID,
 		})
-		if enforce_csrf_check {
+		if enforce_csrf_check{
 			return AccessToken{}, "missing csrf token", http.StatusForbidden, false
 		}
 	} else {
-		if token_claims.AccessToken.Csrf_token != csrf_token {
+		if DecryptAES(EncryptionKey,token_claims.AccessToken.Csrf_token) != csrf_token {
 			token_claims.AccessToken = Authenticate(c, NewUserRow{
 				Column_email: token_claims.AccessToken.Data.Email,
 				Column_uuid:  token_claims.AccessToken.Data.UUID,
 			})
-			if enforce_csrf_check {
+			if enforce_csrf_check{
 				return AccessToken{}, "invalid csrf token", http.StatusForbidden, false
 			}
 		}
 	}
+
+	token_claims.AccessToken.EncryptedData=DecryptAES(EncryptionKey,token_claims.AccessToken.EncryptedData)
 
 	return token_claims.AccessToken, "", http.StatusOK, true
 }
